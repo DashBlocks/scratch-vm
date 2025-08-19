987990bad1edf89ebb13982ff05bb93af62acc2b
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-08-19 18:19:06 +0500

    Update dash_json.js

--- src/blocks/dash_json.js
+++ src/blocks/dash_json.js
@@ -18,14 +18,20 @@ class DashJSONBlocks {
             json_array_empty: this.arrayEmpty,
             json_array_item_of: this.arrayItemOf,
             json_array_item_no_of: this.arrayItemNoOf,
-            json_array_contains: this.arrayContains,
-            json_array_length: this.arrayLength,
+            json_contains: this.contains,
+            json_length: this.length,
             json_array_in_front_of: this.arrayAddFront,
             json_array_behind: this.arrayAddBack,
             json_array_at: this.arrayInsertAt,
             json_array_split: this.arraySplit,
             json_array_delete: this.arrayDelete,
-            json_array_replace: this.arrayReplace
+            json_array_replace: this.arrayReplace,
+            json_object_empty: this.objectEmpty,
+            json_object_split: this.objectSplit,
+            json_object_item_of: this.objectItemOf,
+            json_object_contains_key: this.objectContainsKey,
+            json_object_set: this.objectSet,
+            json_object_delete: this.objectDelete
         };
     }
 
@@ -48,15 +54,15 @@ class DashJSONBlocks {
         return array.indexOf(item) + 1;
     }
 
-    arrayContains (args) {
-        const array = Cast.toList(args.ARRAY);
+    contains (args) {
+        const json = Cast.toJSON(args.JSON, true);
         const item = args.VALUE;
-        return array.includes(item);
+        return Array.isArray(json) ? json.includes(item) : Object.values(json).includes(item);
     }
 
-    arrayLength (args) {
-        const array = Cast.toList(args.VALUE);
-        return array.length;
+    length (args) {
+        const json = Cast.toJSON(args.VALUE, true);
+        return Array.isArray(json) ? json.length : Object.keys(json).length;
     }
 
     arrayAddFront (args) {
@@ -108,6 +114,58 @@ class DashJSONBlocks {
         }
         array[index] = item;
         return array;
+    }
+
+    objectEmpty () {
+        return {};
+    }
+
+    objectSplit (args) {
+        const text = Cast.toString(args.TEXT);
+        const keyDelimiter = Cast.toString(args.KEYDELIM);
+        const pairDelimiter = Cast.toString(args.PAIRDELIM);
+        let error = false;
+        const result = text.split(pairDelimiter).reduce((acc, pair) => {
+            if (!error) {
+                const splitted = pair.split(keyDelimiter);
+                if (splitted.length === 2) {
+                    acc[splitted[0]] = splitted[1];
+                    return acc;
+                }
+                error = true;
+            }
+        }, {});
+        return error ? {} : result;
+    }
+
+    objectItemOf (args) {
+        const object = Cast.toObject(args.VALUE);
+        const key = Cast.toString(args.KEY);
+        if (!Object.keys(object).includes(key)) {
+            return '';
+        }
+        return object[key];
+    }
+
+    objectContainsKey (args) {
+        const object = Cast.toObject(args.OBJECT);
+        const key = Cast.toString(args.KEY);
+        return Object.keys(object).includes(key);
+    }
+
+    objectSet (args) {
+        const object = Cast.toObject(args.OBJECT);
+        const key = Cast.toString(args.KEY);
+        const item = args.ITEM;
+        object[key] = item;
+        return object;
+    }
+
+    objectDelete (args) {
+        const object = Cast.toObject(args.OBJECT);
+        const key = Cast.toString(args.KEY);
+        delete object[key];
+        return object;
     }
 }
 
