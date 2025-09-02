1ec882802ce0c78fdfc1723ffbbe59c16c6fc456
DBDev <dambul2809@mail.ru>
2025-09-02 12:32:56 +0700

    Update scratch3_control.js

--- src/blocks/scratch3_control.js
+++ src/blocks/scratch3_control.js
@@ -1,5 +1,7 @@
 const Cast = require('../util/cast');
-import {isPaused, setPaused} from 'scratch-gui/src/addons/addons/debugger/module';
+// TODO: Fix import path for desktop and web versions
+const {isPaused, setPaused} = require('../../../../src/addons/addons/debugger/module'); // Web
+// const {isPaused, setPaused} = require('../../../scratch-gui/src/addons/addons/debugger/module'); // Desktop
 
 class Scratch3ControlBlocks {
     constructor (runtime) {
