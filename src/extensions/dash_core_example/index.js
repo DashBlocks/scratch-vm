90860d79d100ce169939f56625e2917228660fe7
DBDev <dambul2809@mail.ru>
2025-09-20 02:59:50 +0000

    BlockType.XML => BlockType.LABEL

--- src/extensions/dash_core_example/index.js
+++ src/extensions/dash_core_example/index.js
@@ -75,12 +75,10 @@ class DashCoreExample {
             id: 'coreExample',
             name: 'Dash Core Example', // This string does not need to be translated as this extension is only used as an example.
             blocks: [
-                /*
                 {
-                    blockType: BlockType.XML,
-                    xml: "<sep gap='6'/><label text='Warning: Don't use these blocks'/><sep gap='-12'/><label text='for real projects!'/><sep gap='24'/>",
+                    blockType: BlockType.LABEL,
+                    text: "Warning: Don't use these blocks for real projects!",
                 },
-                */
                 {
                     func: 'MAKE_A_VARIABLE',
                     blockType: BlockType.BUTTON,
