908c381f8ee6b082a2ea6edee57225e57e22065f
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-10-04 23:14:45 +0500

    Update jsexecute.js

--- src/compiler/jsexecute.js
+++ src/compiler/jsexecute.js
@@ -604,25 +604,40 @@ runtimeFunctions.listContents = `const listContents = list => {
 }`;
 
 /**
+ * Replace a value in an array.
+ * @param {Array} array The array.
+ * @param {*} idx The Scratch index in the array.
+ * @param {*} value The new value.
+ * @returns {Array} The new array
+ */
+runtimeFunctions.arrayReplace = `const arrayReplace = (array, idx, value) => {
+    const index = listIndex(idx, array.length);
+    if (index === -1) {
+        return array;
+    }
+    return [...array.slice(0, index), value, ...array.slice(index)];
+}`;
+
+/**
  * Insert a value in an array.
  * @param {Array} array The array.
  * @param {*} idx The Scratch index in the array.
  * @param {*} value The value to insert.
- * @returns {Array} New array
+ * @returns {Array} The new array
  */
 runtimeFunctions.arrayInsert = `const arrayInsert = (array, idx, value) => {
     const index = listIndex(idx, array.length + 1);
     if (index === -1) {
         return array;
     }
-    return list.toSpliced(index, 0, value);
+    return array.toSpliced(index, 0, value);
 }`;
 
 /**
  * Delete a value from an array.
  * @param {Array} array The array.
  * @param {*} idx The Scratch index in the array.
- * @returns {Array} New array
+ * @returns {Array} The new array
  */
 runtimeFunctions.arrayDelete = `const arrayDelete = (array, idx, value) => {
     if (idx === 'all') {
@@ -632,7 +647,7 @@ runtimeFunctions.arrayDelete = `const arrayDelete = (array, idx, value) => {
     if (index === -1) {
         return array;
     }
-    return list.toSpliced(index, 1);
+    return array.toSpliced(index, 1);
 }`;
 
 /**
