c33c596f86c9e3ded269e32ba0be0d6414cd785e
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-10-27 17:19:37 +0500

    Update irgen.js

--- src/compiler/irgen.js
+++ src/compiler/irgen.js
@@ -521,18 +521,15 @@ class ScriptTreeGenerator {
                 right: this.descendInputOfBlock(block, 'NUM2').toType(InputType.NUMBER)
             });
         case 'operator_mathexpandable': {
-            const menuOperators = block.mutation.menuvalues;
-            const inputs = Object.values(block.inputs);
-            const operations = [];
-            for (var i = 0; i < inputs.length; i++) {
-                const input = inputs[i];
-                if (input.block == null) delete block.inputs[input.name];
-                else operations.push([
-                  this.descendInputOfBlock(block, input.name),
-                  menuOperators[i]
-                ]);
+            const inputs = [];
+            for (const input of Object.values(block.inputs)) {
+                if (input.block == null) {
+                    delete block.inputs[input.name];
+                } else {
+                    inputs.push(this.descendInputOfBlock(block, input.name));
+                }
             }
-            return new IntermediateInput(InputOpcode.OP_MATH, InputType.NUMBER_OR_NAN, {operations});
+            return new IntermediateInput(InputOpcode.OP_MATH, InputType.NUMBER_OR_NAN, {inputs});
         }
         case 'operator_not':
             return new IntermediateInput(InputOpcode.OP_NOT, InputType.BOOLEAN, {
