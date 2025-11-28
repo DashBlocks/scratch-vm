7072469c93cabd051539991f1d257e511db823d1
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2025-11-28 19:53:01 +0500

    Update scratch3_procedures.js

--- src/blocks/scratch3_procedures.js
+++ src/blocks/scratch3_procedures.js
@@ -17,7 +17,9 @@ class Scratch3ProcedureBlocks {
             procedures_call: this.call,
             procedures_return: this.return,
             argument_reporter_string_number: this.argumentReporterStringNumber,
-            argument_reporter_boolean: this.argumentReporterBoolean
+            argument_reporter_boolean: this.argumentReporterBoolean,
+            argument_reporter_array: this.argumentReporterArray,
+            argument_reporter_object: this.argumentReporterObject
         };
     }
 
@@ -125,6 +127,26 @@ class Scratch3ProcedureBlocks {
             if (lowercaseValue === 'is dash?' || lowercaseValue === 'is turbowarp?') {
                 return true;
             }
+            // When the parameter is not found in the most recent procedure
+            // call, the default is always 0.
+            return 0;
+        }
+        return value;
+    }
+
+    argumentReporterArray (args, util) {
+        const value = util.getParam(args.VALUE);
+        if (value === null) {
+            // When the parameter is not found in the most recent procedure
+            // call, the default is always 0.
+            return 0;
+        }
+        return value;
+    }
+
+    argumentReporterObject (args, util) {
+        const value = util.getParam(args.VALUE);
+        if (value === null) {
             // When the parameter is not found in the most recent procedure
             // call, the default is always 0.
             return 0;
