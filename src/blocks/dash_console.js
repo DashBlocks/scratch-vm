ac09fc4b4f8ba65429f2021439c4434ca9805cd0
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-01-19 22:06:24 +0500

    Update dash_console.js

--- src/blocks/dash_console.js
+++ src/blocks/dash_console.js
@@ -17,6 +17,10 @@ class DashConsoleBlocks {
         return {
             console_clear: this.clear,
             console_addline: this.addLine,
+            console_addlineandmove: this.addLineAndMove,
+            console_editline: this.editLine,
+            console_editsymbol: this.editSymbol,
+            console_movecursor: this.moveCursor,
             console_of: this.getAttributeOf
         };
     }
@@ -30,7 +34,39 @@ class DashConsoleBlocks {
     addLine (args) {
         if (this.runtime.console) {
             const line = Cast.toString(args.LINE);
-            this.runtime.console.addLine(line);
+            this.runtime.console.addLine(line, false);
+        }
+    }
+
+    addLineAndMove (args) {
+        if (this.runtime.console) {
+            const line = Cast.toString(args.LINE);
+            this.runtime.console.addLine(line, true);
+        }
+    }
+
+    editLine (args) {
+        if (this.runtime.console) {
+            const line = Cast.toString(args.LINE);
+            this.runtime.console.editLine(line);
+        }
+    }
+
+    editSymbol (args) {
+        if (this.runtime.console) {
+            const value = Cast.toString(args.VALUE);
+            this.runtime.console.editSymbol(value);
+        }
+    }
+
+    moveResult (args) {
+        if (this.runtime.console) {
+            const row = Cast.toListIndex(args.ROW, this.runtime.console.state.linesCount, false);
+            const symbol = Cast.toListIndex(args.SYMBOL, this.runtime.console.state.symbols, false);
+            if (row === Cast.LIST_INVALID || symbol === Cast.LIST_INVALID) {
+                return;
+            }
+            this.runtime.console.props.setConsoleCursor(row - 1, symbol - 1);
         }
     }
 
