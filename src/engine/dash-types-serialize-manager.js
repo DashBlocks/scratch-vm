e48a9df93d75671af6259697dcce4ff4ed352d19
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-03-21 22:34:40 +0500

    Write deserialize method

--- src/engine/dash-types-serialize-manager.js
+++ src/engine/dash-types-serialize-manager.js
@@ -7,8 +7,7 @@
 const isValueSafeForJSON = value => (
     typeof value === 'number' ||
     typeof value === 'string' ||
-    typeof value === 'boolean' ||
-    value == null
+    typeof value === 'boolean'
 );
 
 const isGenerator = obj => (
@@ -89,14 +88,14 @@ class TypesSerializeManager {
                     value?.constructor?.prototype === Object.prototype
                 ) {
                     actions.unshift([
-                        this._serializers.json_json.serialize(),
+                        this._serializers.json_json.serialize(value),
                         fn4serializedWrapper(value)
                     ]);
                 } else if (typeof value?.customId === 'string') {
                     if (!(value.customId in this._serializers))
                         throw new Error(`Unknown serializer of custom type with id: ${value.customId}`);
                     actions.unshift([
-                        this._serializers[value.customId].serialize(),
+                        this._serializers[value.customId].serialize(value),
                         fn4serializedWrapper(value)
                     ]);
                 } else if (!isValueSafeForJSON(value)) {
@@ -129,14 +128,52 @@ class TypesSerializeManager {
     }
 
     deserialize (value) {
-        
+        const actions = [];
+        const go2Prev = false;
+        do {
+            if (!go2Prev) {
+                if (!(typeof value === "object" && value instanceof Object)) {
+                    go2Prev = true;
+                    continue;
+                } else if (Array.isArray(value)) {
+                    actions.unshift(this._serializers.json_json.deserialize(value));
+                } else if ('customType' in value) {
+                    if (!value.customType) {
+                        actions.unshift(this._serializers.json_json.deserialize(value.serialized));
+                    } else if (value.typeId in this._serializers) {
+                        actions.unshift(this._serializers[value.customId].deserialize(value.serialized));
+                    } else {
+                        throw new Error(`Unknown deserializer of custom type with id: ${value.customId}`);
+                    }
+                } else {
+                    actions.unshift(this._serializers.json_json.deserialize(value));
+                }
+            }
+            go2Prev = false;
+            const gen = actions[0];
+            if (isGenerator(gen)) {
+                resultOfNext = gen.next(value);
+                if (resultOfNext.done) {
+                    value = resultOfNext.value;
+                    go2Prev = true;
+                    actions.splice(0, 1);
+                } else {
+                    value = resultOfNext.value;
+                }
+            } else {
+                value = gen;
+                go2Prev = true;
+                actions.splice(0, 1);
+            }
+        } while (actions.length > 0)
+        return value;
     }
 
     registerSerializer (id, serialize, deserialize) {
-        if (typeof id !== 'string') throw new TypeError('ID must be of type string');
-        if (id === 'json_json') throw new TypeError('json_json is a special serializer');
-        if (typeof serialize !== 'function') throw new TypeError('Serialize must be of type function');
-        if (typeof deserialize !== 'function') throw new TypeError('Deserialize must be of type function');
+        if (typeof id !== 'string') throw new TypeError('id is not a string');
+        if (id === 'json_json') throw new TypeError('json_json is a special reserved serializer');
+        if (typeof serialize !== 'function') throw new TypeError('serialize is not a function');
+        if (typeof deserialize !== 'function') throw new TypeError('deserialize is not a function');
         this._serializers[id] = {serialize, deserialize};
     }
 }
