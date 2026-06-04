67cb18b3b88d1078248541126248c8c5399a38a4
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-06-04 12:57:43 +0500

    scratch3_control.js: 🔧 Changes in target substitute for `control_run_as` block

--- src/blocks/scratch3_control.js
+++ src/blocks/scratch3_control.js
@@ -274,15 +274,12 @@ class Scratch3ControlBlocks {
         // If runner target is not found, return
         if (!runnerTarget) return;
         
-        // If the runner target is identical to the current one,
-        // then just run branch without spoofing.
-        if (runnerTarget === util.target) {
-            util.startBranch(1, false);
-        } else {
-            util.thread.peekStackFrame().spoofTarget = util.target;
-            util.startBranch(1, false);
-            util.thread.target = runnerTarget;
+        // If the runner target is not identical to the current one,
+        // then target substitute is required.
+        if (runnerTarget !== util.target) {
+            util.thread.peekStackFrame().substituteTarget = runnerTarget;
         }
+        util.startBranch(1, false);
     }
 }
 
