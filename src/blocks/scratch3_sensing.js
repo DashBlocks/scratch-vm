d72178d894cdbb60bb79dce0c8c3f94e334db85f
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-08-06 23:04:01 +0500

    scratch3_sensing.js: ✨ I forgot this change

--- src/blocks/scratch3_sensing.js
+++ src/blocks/scratch3_sensing.js
@@ -369,7 +369,7 @@ class Scratch3SensingBlocks {
             switch (args.PROPERTY) {
             case 'x position': return attrTarget.x;
             case 'y position': return attrTarget.y;
-            case 'position': return [attrTarget.x, attrTarget.y];
+            case 'position': return new NormalArray([attrTarget.x, attrTarget.y]);
             case 'direction': return attrTarget.direction;
             case 'visibility': return attrTarget.visible;
             case 'costume #': return attrTarget.currentCostume + 1;
