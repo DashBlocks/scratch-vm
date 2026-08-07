26cc5e1939f7af2d1cabf76bf375d9b3a72753d2
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-08-07 12:45:19 +0500

    tw-extension-api-common.js: ⬆ Update

--- src/extension-support/tw-extension-api-common.js
+++ src/extension-support/tw-extension-api-common.js
@@ -5,8 +5,8 @@ const TargetType = require('./target-type');
 const Cast = require('../util/cast');
 const Patcher = require('../util/patcher');
 const SandboxRunner = require('../util/sandboxed-javascript-runner');
-const NormalArray = require('./data-types/dash-normal-array');
-const NormalObject = require('./data-types/dash-normal-object');
+const NormalArray = require('../data-types/dash-normal-array');
+const NormalObject = require('../data-types/dash-normal-object');
 const external = require('./tw-external');
 
 const Scratch = {
